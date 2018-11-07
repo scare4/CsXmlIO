93900f4fab013a3ef1c466143322231ef892cd5a
nmarinpache <nicolas.marin-pache@etu.u-bordeaux.fr>
2018-11-07 21:00:16 +0100

    updates

--- XmlIO/ListIO.cs
+++ XmlIO/ListIO.cs
@@ -18,6 +18,15 @@ namespace XmlIO
             }
         }
 
+        public static void WriteList<T>(string path, List<T> list, string root)
+        {
+            using (var Writer = new FileStream(path, FileMode.Create))
+            {
+                XmlSerializer Serializer = new XmlSerializer(typeof(List<T>), new XmlRootAttribute(root));
+                Serializer.Serialize(Writer, list);
+            }
+        }
+
         //Reads list from an xml file
         public static List<T> ReadList<T>(string path)
         {
@@ -25,6 +34,17 @@ namespace XmlIO
             using (var Reader = new StreamReader(path))
             {
                 XmlSerializer Deserializer = new XmlSerializer(typeof(List<T>), new XmlRootAttribute("product_list"));
+                list = (List<T>)Deserializer.Deserialize(Reader);
+            }
+            return list;
+        }
+
+        public static List<T> ReadList<T>(string path, string root)
+        {
+            List<T> list;
+            using (var Reader = new StreamReader(path))
+            {
+                XmlSerializer Deserializer = new XmlSerializer(typeof(List<T>), new XmlRootAttribute(root));
                 list = (List<T>)Deserializer.Deserialize(Reader);
             }
             return list;
---
+++ XmlIO/ObjectIO.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace XmlIO
+{
+    public class ObjectIO
+    {
+        public static void WriteObject<T>(string path, T Object, string root)
+        {
+            using (var Writer = new FileStream(path, FileMode.Create))
+            {
+                XmlSerializer Serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(root));
+                Serializer.Serialize(Writer, Object);
+            }
+        }
+        
+        public static object ReadObject(string path, string root)
+        {
+            object Object;
+            using (var Reader = new StreamReader(path))
+            {
+                XmlSerializer Deserializer = new XmlSerializer(typeof(object), new XmlRootAttribute(root));
+                Object = Deserializer.Deserialize(Reader);
+            }
+            return Object;
+        }
+    }
+}
